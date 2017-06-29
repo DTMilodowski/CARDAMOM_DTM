dd0536b9a3323a0a14849346f2559dd9d87e77ce
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-06-29 16:05:40 +0100

    get plot coords

--- src/load_data.py
+++ src/load_data.py
@@ -2,8 +2,13 @@
 
 import numpy as np
 
+def load_plot_coordinates(coordinate_file):
+    dtypes = {'names': ('plot','lat','long'),'formats':('string', 'float', 'float')}
+    info = np.genfromtxt(met_file,skiprows=1,delimiter=',',dtype=dtypes)
+    return info
+
 def load_met_data(met_file):
-    met_dtype = {'names': ('tstep_days','date','mn2t','mx2t','vpd','ssrd','pptn','mn2t_21d','mx2t_21d','vpd_21d','ssrd_21d','pptn_21d'),'formats':('int', 'datetime64[D]', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float')}
+    met_dtype = {'names': ('tstep_days','date','mn2t','mx2t','vpd','ssrd','pptn','mn2t_21d','mx2t_21d','vpd_21d','ssrd_21d','pptn_21d'),'formats':('int', 'string', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float')}
     met_data = np.genfromtxt(met_file,skiprows=1,delimiter=',',dtype=met_dtype)
     return met_data
     
