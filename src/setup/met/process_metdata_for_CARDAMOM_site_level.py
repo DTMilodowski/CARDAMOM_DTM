6312cd2d5c12a3b0b35622d0b837a1113ae92a87
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-07-26 15:59:24 +0100

    new function dedicated to setting up site level met data for CARDAMOM

---
+++ src/setup/met/process_metdata_for_CARDAMOM_site_level.py
@@ -0,0 +1,15 @@
+# This code sets up the met drivers for CARDAMOM
+#
+# CARDAMOM-DALEC is driven with remotely sensed meteorologoical data - from ERA-Interim Reanalysis and TRMM.
+# At the plot level, I do not generally include fire and/or deforestation unless directly observed in the
+# field campaigns - this is not observed at BALI sites
+
+# import some libraries - update as required
+import numpy as np
+import sys
+sys.path.append('/home/dmilodow/DataStore_DTM/BALI/SPA_BALI_data_and_analysis/scripts/CARDAMOM/CARDAMOM_setup_drivers/')
+import met_setup as met
+import MODIS_setup as MODIS
+import field_data_setup as field
+sys.path.append('/home/dmilodow/DataStore_DTM/BALI/SPA_BALI_data_and_analysis/scripts/construct_drivers/')
+import gapfill_station_metdata as gap
