d7b46daf8d22a91850bb5baa7b067aaa44abaf9d
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-20 16:18:05 +0100

    selecting model pools

--- src/rerun/python/analyse_GEMplot_rerun_DALEC_GSI_DFOL_CWD_FR.py
+++ src/rerun/python/analyse_GEMplot_rerun_DALEC_GSI_DFOL_CWD_FR.py
@@ -25,6 +25,20 @@ filename = 'BALI_GEMplots_daily_2011_2016_DALEC_GSI_DFOL_CWD_FR.nc'
 
 # find NetCDF_file and load
 NetCDF_file = '%s%s/rerun/%s/%s' % (path2project,project,run,filename)
-dataset = Dataset(NetCDF_file)
+mod = Dataset(NetCDF_file)
+
+site_index = 0
+mod_pools = {}
+mod_pools['Cwoo']=mod.variables['Cwoo'][:,:,0,0]
+mod_pools['Croo']=mod.variables['Croo'][:,:,0,0]
+mod_pools['Clit']=mod.variables['Clit'][:,:,0,0]
+mod_pools['Csom']=mod.variables['Csom'][:,:,0,0]
+mod_pools['Ccwd']=mod.variables['Ccwd'][:,:,0,0]
+mod_pools['Clab']=mod.variables['Clab'][:,:,0,0]
+mod_pools['Cfol']=mod.variables['Cfol'][:,:,0,0]
+mod_pools['time']=mod.variables['time']
+
+obs_pools={}
 
 # plot Carbon stocks
+pCAR.plot_carbon_pools_ts(mod_pools,obs_pools)
