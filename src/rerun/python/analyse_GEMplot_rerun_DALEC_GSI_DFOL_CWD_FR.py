1fec1d2b84154eeecb3167b286070e98271a5fd8
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-20 14:45:59 +0100

    loading file

--- src/rerun/python/analyse_GEMplot_rerun_DALEC_GSI_DFOL_CWD_FR.py
+++ src/rerun/python/analyse_GEMplot_rerun_DALEC_GSI_DFOL_CWD_FR.py
@@ -13,10 +13,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from netCDF4 import Dataset
 
+path2project = '/home/dmilodow/DataStore_DTM/BALI/CARDAMOM_BALI/projects/'
+project = 'BALI_GEMplots_daily'
+run = '001'
 
-
-
-
+# find NetCDF_file and load
+NetCDF_file = '%s%s/rerun/%s' % (path2project,project,run)
 dataset = Dataset(NetCDF_file)
 
 # Get the spatial information from the layer
