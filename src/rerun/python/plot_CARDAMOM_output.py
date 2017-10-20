412433404d7ccf17b66c66844c4c873f860c5aa5
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-20 15:21:48 +0100

    pools template

--- src/rerun/python/plot_CARDAMOM_output.py
+++ src/rerun/python/plot_CARDAMOM_output.py
@@ -7,3 +7,21 @@
 # Author: D.T.Milodowski
 # Date: October 2017
 #===============================================================================
+import numpy as np
+from matplotlib import pyplot as plt
+
+#---------------------
+# plot_carbon_pools_ts
+# --------------------
+# Plot a time series of C stocks, with observations if available.
+# The function reads in two dictionaries:
+# - the model output (median, ulim, llim)
+# - the obserations where available
+# Note that these dictionaries should have uniform naming structures for the
+# carbon pools (woo,fol,lab,roo,lit,cwd,som)
+# Also takes optional arguments for start and end timestep. initially these will
+# be index references (i.e. model timestep) but this will ultimately be altered
+# to give options to specify date ranges.
+def plot_carbon_pools_ts(model,obs,start_tstep=0,end_tstep=-1):
+
+    return 0
