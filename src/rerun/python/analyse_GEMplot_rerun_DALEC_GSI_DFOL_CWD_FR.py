d09824111867b9c1ccf9548e6bf10c3263fb6bd0
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-20 14:09:46 +0100

    new script for diagnostic GEM analysis

---
+++ src/rerun/python/analyse_GEMplot_rerun_DALEC_GSI_DFOL_CWD_FR.py
@@ -0,0 +1,11 @@
+#===============================================================================
+# analyse_GEMplot_rerun_DALEC_GSI_DFOL_CWD_FR.py
+#-------------------------------------------------------------------------------
+# This script analyses output from the GEM plot CARDAMOM runs using the model
+# DALEC_GSI_DFOL_CWD_FR. Analysis includes:
+# - diagnostic plots against GEM plot survey data
+# - (to be added as required)
+#-------------------------------------------------------------------------------
+# Author: D.T.Milodowski
+# Date: October 2017
+#===============================================================================
