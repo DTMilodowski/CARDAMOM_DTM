0d55ef31e9edae7ff716a7c4f8ee41d353b775fd
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-20 15:37:36 +0100

    subplot template

--- src/rerun/python/plot_CARDAMOM_output.py
+++ src/rerun/python/plot_CARDAMOM_output.py
@@ -9,6 +9,13 @@
 #===============================================================================
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import rcParams
+# Set up some basiic parameters for the plots
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['arial']
+rcParams['font.size'] = 8
+rcParams['legend.numpoints'] = 1
+axis_size = rcParams['font.size']+2
 
 #---------------------
 # plot_carbon_pools_ts
@@ -23,5 +30,34 @@ from matplotlib import pyplot as plt
 # be index references (i.e. model timestep) but this will ultimately be altered
 # to give options to specify date ranges.
 def plot_carbon_pools_ts(model,obs,start_tstep=0,end_tstep=-1):
+    fig = plt.figure(1, facecolor='White',figsize=[10,8])
+
+    # Plot a -> Cwood
+    ax1a = plt.subplot2grid((7,1),(0,0))
+    ax1a.annotate('a - C$_{wood}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    
+    # Plot b -> Cfol
+    ax1b = plt.subplot2grid((7,1),(1,0))
+    ax1b.annotate('b - C$_{foliar}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    
+    # Plot c -> Croot
+    ax1c = plt.subplot2grid((7,1),(2,0))
+    ax1c.annotate('c - C$_{root}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    
+    # Plot d -> Clab
+    ax1d = plt.subplot2grid((7,1),(3,0))
+    ax1d.annotate('d - C$_{labile}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    
+    # Plot e -> Clit
+    ax1e = plt.subplot2grid((7,1),(4,0))
+    ax1e.annotate('e - C$_{litter}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    
+    # Plot f -> Ccwd
+    ax1f = plt.subplot2grid((7,1),(3,0))
+    ax1f.annotate('f - C$_{cwd}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    
+    # Plot g -> Csom
+    ax1g = plt.subplot2grid((7,1),(4,0))
+    ax1g.annotate('g - C$_{SOM}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
 
     return 0
