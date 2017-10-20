2c76f3b291d3acdcc2f80af529425563ca7a9948
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-20 14:51:30 +0100

    to host standard CARDAMOM plotting scripts

---
+++ src/rerun/python/plot_CARDAMOM_output.py
@@ -0,0 +1,9 @@
+#===============================================================================
+# plot_CARDAMOM_output.py
+#-------------------------------------------------------------------------------
+# This script contains functions to produce standard plots of CARDAMOM output,
+# such as carbon pools through time in addition to data where present
+#-------------------------------------------------------------------------------
+# Author: D.T.Milodowski
+# Date: October 2017
+#===============================================================================
