2d05ae3d6ec6ce07eb77f67a856c14b023707923
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-20 16:17:47 +0100

    basic stocks plot, no observations yet

--- src/rerun/python/plot_CARDAMOM_output.py
+++ src/rerun/python/plot_CARDAMOM_output.py
@@ -16,6 +16,7 @@ rcParams['font.sans-serif'] = ['arial']
 rcParams['font.size'] = 8
 rcParams['legend.numpoints'] = 1
 axis_size = rcParams['font.size']+2
+colour = ['#46E900','#1A2BCE','#E0007F']
 
 #---------------------
 # plot_carbon_pools_ts
@@ -25,7 +26,7 @@ axis_size = rcParams['font.size']+2
 # - the model output (median, ulim, llim)
 # - the obserations where available
 # Note that these dictionaries should have uniform naming structures for the
-# carbon pools (woo,fol,lab,roo,lit,cwd,som)
+# carbon pools (Cwoo,Cfol,Clab,Croo,Clit,Ccwd,Csom)
 # Also takes optional arguments for start and end timestep. initially these will
 # be index references (i.e. model timestep) but this will ultimately be altered
 # to give options to specify date ranges.
@@ -37,42 +38,65 @@ def plot_carbon_pools_ts(model,obs,start_tstep=False,end_tstep=False):
     ax1a.annotate('a - C$_{wood}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
     ax1a.set_ylabel('C$_{wood}$ / g(C) m$^{-2}$',fontsize = axis_size)
     
+    ax1a.fill_between(model['time'],model['Cwoo'][:,3],model['Cwoo'][:,4],color=colour[0],alpha=0.2)
+    ax1a.plot(model['time'],model['Cwoo'][:,1],'-',color=colour[0])
+    
     # Plot b -> Cfol
     ax1b = plt.subplot2grid((7,1),(1,0),sharex=ax1a)
     ax1b.annotate('b - C$_{foliar}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
     ax1b.set_ylabel('C$_{fol}$ / g(C) m$^{-2}$',fontsize = axis_size)
+    
+    ax1b.fill_between(model['time'],model['Cfol'][:,3],model['Cfol'][:,4],color=colour[0],alpha=0.2)
+    ax1b.plot(model['time'],model['Cfol'][:,1],'-',color=colour[0])
     
     # Plot c -> Croot
     ax1c = plt.subplot2grid((7,1),(2,0),sharex=ax1a)
     ax1c.annotate('c - C$_{root}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
     ax1c.set_ylabel('C$_{root}$ / g(C) m$^{-2}$',fontsize = axis_size)
     
+    ax1c.fill_between(model['time'],model['Croo'][:,3],model['Croo'][:,4],color=colour[0],alpha=0.2)
+    ax1c.plot(model['time'],model['Croo'][:,1],'-',color=colour[0])
+    
     # Plot d -> Clab
     ax1d = plt.subplot2grid((7,1),(3,0),sharex=ax1a)
     ax1d.annotate('d - C$_{labile}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
     ax1d.set_ylabel('C$_{lab}$ / g(C) m$^{-2}$',fontsize = axis_size)
+    
+    ax1d.fill_between(model['time'],model['Clab'][:,3],model['Clab'][:,4],color=colour[0],alpha=0.2)
+    ax1d.plot(model['time'],model['Clab'][:,1],'-',color=colour[0])
     
     # Plot e -> Clit
     ax1e = plt.subplot2grid((7,1),(4,0),sharex=ax1a)
     ax1e.annotate('e - C$_{litter}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
     ax1e.set_ylabel('C$_{lit}$ / g(C) m$^{-2}$',fontsize = axis_size)
     
+    ax1e.fill_between(model['time'],model['Clit'][:,3],model['Clit'][:,4],color=colour[1],alpha=0.2)
+    ax1e.plot(model['time'],model['Clit'][:,1],'-',color=colour[1])
+    
     # Plot f -> Ccwd
     ax1f = plt.subplot2grid((7,1),(5,0),sharex=ax1a)
     ax1f.annotate('f - C$_{cwd}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
     ax1f.set_ylabel('C$_{cwd}$ / g(C) m$^{-2}$',fontsize = axis_size)
+    
+    ax1f.fill_between(model['time'],model['Ccwd'][:,3],model['Ccwd'][:,4],color=colour[1],alpha=0.2)
+    ax1f.plot(model['time'],model['Ccwd'][:,1],'-',color=colour[1])
     
     # Plot g -> Csom
     ax1g = plt.subplot2grid((7,1),(6,0),sharex=ax1a)
     ax1g.annotate('g - C$_{SOM}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
     ax1g.set_ylabel('C$_{som}$ / g(C) m$^{-2}$',fontsize = axis_size)
     ax1g.set_xlabel('timestep',fontsize = axis_size)
+    
+    ax1g.fill_between(model['time'],model['Csom'][:,3],model['Csom'][:,4],color=colour[1],alpha=0.2)
+    ax1g.plot(model['time'],model['Csom'][:,1],'-',color=colour[1])
 
     # set xlimits if desired
     if start_tstep!=False:
         ax1a.set_xlim(xmin=start_tstep)
     if end_tstep!=False:
         ax1a.set_xlim(xmax=end_tstep)
+    else:
+        ax1a.set_xlim(xmax=model['time'].size)
 
     plt.tight_layout()
     plt.show()
