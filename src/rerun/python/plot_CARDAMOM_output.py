446e3545f57012bed8d3b878bfcb56b1791e0dc5
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-11-06 16:45:45 +0000

    stopping for the day midway through finishing plotting script

--- src/rerun/python/plot_CARDAMOM_output.py
+++ src/rerun/python/plot_CARDAMOM_output.py
@@ -177,14 +177,126 @@ def plot_carbon_pools_ts(model,obs,start_tstep=False,end_tstep=False):
 # Note that these dictionaries should have uniform naming structures
 # - lai : leaf area index
 # - gsi : growth season index
-# - Cfol : litter stocks
+# - Clit : litter stocks
 # - Croo : root carbon stocks
 # - Ccwd : coarse woody debris stocks
 # - flux_fol_lit : carbon flux from foliar pool to litter (litterfall)
-# - flux_cwd_lit : carbon flux from CWD pool to litter
-# - flux_root_lit : carbon flux from root pool to litter
-# - Rh_lit : heterotrophic respiration from litter pool
-# - decomp_lit : decomposition flux of carbon from litter to SOM
+# - flux_cwd_lit* : carbon flux from CWD pool to litter
+# - flux_root_lit* : carbon flux from root pool to litter
+# - Rh_lit** : heterotrophic respiration from litter pool
+# - decomp_lit** : decomposition flux of carbon from litter to SOM
+# fields marked * and ** are combined for brevity in plotting
 # Also takes optional arguments for start and end timestep. initially these will
 # be index references (i.e. model timestep) but this will ultimately be altered
 # to give options to specify date ranges.
+def plot_litter_components_ts(model,obs,start_tstep=False,end_tstep=False):
+    fig = plt.figure(3, facecolor='White',figsize=[8,14])
+
+    # Plot a -> LAI
+    ax1a = plt.subplot2grid((8,1),(0,0))
+    ax1a.annotate('a - LAI$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    ax1a.set_ylabel('LAI / m$^2$m$^{-2}$',fontsize = axis_size)
+    
+    ax1a.fill_between(model['time'],model['lai'][:,3],model['lai'][:,4],color=colour[0],alpha=0.2)
+    ax1a.plot(model['time'],model['lai'][:,1],'-',color=colour[0])
+
+    if 'lai' in obs.keys(): # check for observations
+        if 'lai' in obs.keys(): # check for uncertainty bounds
+            ax1a.error_bar(obs['time'],obs['lai'],yerr=obs['lai_u'],marker='o',c='black',mec='black',mfc='black',ecolor='black')
+        else:
+            ax1a.plot(obs['time'],obs['lai'],marker='o',c='black',mec='black',mfc='black')
+
+    
+    # Plot b -> gsi
+    ax1b = plt.subplot2grid((8,1),(1,0),sharex=ax1a)
+    ax1b.annotate('b - Growth Season Index (GSI)', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    ax1b.set_ylabel('GSI',fontsize = axis_size)
+    
+    ax1b.fill_between(model['time'],model['gsi'][:,3],model['gsi'][:,4],color=colour[0],alpha=0.2)
+    ax1b.plot(model['time'],model['gsi'][:,1],'-',color=colour[0])
+    
+    
+    # Plot c -> Clit
+    ax1c = plt.subplot2grid((8,1),(2,0),sharex=ax1a)
+    ax1c.annotate('e - C$_{litter}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    ax1c.set_ylabel('C$_{lit}$ / g(C) m$^{-2}$',fontsize = axis_size)
+    
+    ax1c.fill_between(model['time'],model['Clit'][:,3],model['Clit'][:,4],color=colour[1],alpha=0.2)
+    ax1c.plot(model['time'],model['Clit'][:,1],'-',color=colour[1])
+    
+    if 'Clit' in obs.keys(): # check for observations
+        if 'Clit_u' in obs.keys(): # check for uncertainty bounds
+            ax1c.error_bar(obs['time'],obs['Clit'],yerr=obs['Clit_u'],marker='o',c='black',mec='black',mfc='black',ecolor='black')
+        else:
+            ax1c.plot(obs['time'],obs['Clit'],marker='o',c='black',mec='black',mfc='black')
+
+            
+    # Plot d -> Croot
+    ax1d = plt.subplot2grid((8,1),(3,0),sharex=ax1a)
+    ax1d.annotate('c - C$_{root}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    ax1d.set_ylabel('C$_{root}$ / g(C) m$^{-2}$',fontsize = axis_size)
+    
+    ax1d.fill_between(model['time'],model['Croo'][:,3],model['Croo'][:,4],color=colour[0],alpha=0.2)
+    ax1d.plot(model['time'],model['Croo'][:,1],'-',color=colour[0])
+    
+    if 'Croo' in obs.keys(): # check for observations
+        if 'Cwro_u' in obs.keys(): # check for uncertainty bounds
+            ax1d.error_bar(obs['time'],obs['Croo'],yerr=obs['Croo_u'],marker='o',c='black',mec='black',mfc='black',ecolor='black')
+        else:
+            ax1d.plot(obs['time'],obs['Croo'],marker='o',c='black',mec='black',mfc='black')
+    
+    # Plot e -> Ccwd
+    ax1e = plt.subplot2grid((8,1),(4,0),sharex=ax1a)
+    ax1e.annotate('d - C$_{cwd}$', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    ax1e.set_ylabel('C$_{cwd}$ / g(C) m$^{-2}$',fontsize = axis_size)
+    
+    ax1e.fill_between(model['time'],model['Ccwd'][:,3],model['Ccwd'][:,4],color=colour[0],alpha=0.2)
+    ax1e.plot(model['time'],model['Ccwd'][:,1],'-',color=colour[0])
+
+    if 'Ccwd' in obs.keys(): # check for observations
+        if 'Ccwd_u' in obs.keys(): # check for uncertainty bounds
+            ax1e.error_bar(obs['time'],obs['Ccwd'],yerr=obs['Ccwd_u'],marker='o',c='black',mec='black',mfc='black',ecolor='black')
+        else:
+            ax1e.plot(obs['time'],obs['Ccwd'],marker='o',c='black',mec='black',mfc='black')
+            
+    # Plot f -> Cfol into Clit
+    ax1f = plt.subplot2grid((8,1),(5,0),sharex=ax1a)
+    ax1f.annotate('f - litterfall', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    ax1f.set_ylabel('litter flux from foliage / g(C) m$^{-2}$ d$^{-1}$',fontsize = axis_size)
+    
+    ax1f.fill_between(model['time'],model['flux_fol_lit'][:,3],model['flux_fol_lit'][:,4],color=colour[1],alpha=0.2)
+    ax1f.plot(model['time'],model['flux_fol_lit'][:,1],'-',color=colour[1])
+    
+    if 'flux_fol_lit' in obs.keys(): # check for observations
+        if 'flux_fol_lit_u' in obs.keys(): # check for uncertainty bounds
+            ax1f.error_bar(obs['time'],obs['flux_fol_lit'],yerr=obs['flux_fol_lit_u'],marker='o',c='black',mec='black',mfc='black',ecolor='black')
+        else:
+            ax1f.plot(obs['time'],obs['flux_fol_lit'],marker='o',c='black',mec='black',mfc='black')
+
+    # Plot g -> other fluxes into litter pool
+    ax1g = plt.subplot2grid((8,1),(6,0),sharex=ax1a)
+    ax1g.annotate('f - litter flux from CWD and roots', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    ax1g.set_ylabel('litter flux from cwd and roots / g(C) m$^{-2}$ d$^{-1}$',fontsize = axis_size)
+    
+    ax1g.fill_between(model['time'],model['flux_fol_lit'][:,3],model['flux_fol_lit'][:,4],color=colour[1],alpha=0.2)
+    ax1g.plot(model['time'],model['flux_fol_lit'][:,1],'-',color=colour[1])
+
+
+    # Plot h -> litter loss fluxes
+    ax1h = plt.subplot2grid((8,1),(7,0),sharex=ax1a)
+    ax1h.annotate('f - litter loss fluxes', xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',horizontalalignment='left', verticalalignment='top', fontsize=10)
+    ax1h.set_ylabel('litter loss / g(C) m$^{-2}$ d$^{-1}$',fontsize = axis_size)
+    
+    ax1h.fill_between(model['time'],model['Rh_lit'][:,3],model['Rh_lit'][:,4],color=colour[1],alpha=0.2)
+    ax1h.plot(model['time'],model['Rh_lit'][:,1],'-',color=colour[1])
+
+    # set xlimits if desired
+    if start_tstep!=False:
+        ax1a.set_xlim(xmin=start_tstep)
+    if end_tstep!=False:
+        ax1a.set_xlim(xmax=end_tstep)
+    else:
+        ax1a.set_xlim(xmax=model['time'].size)
+
+    plt.tight_layout()
+    plt.show()
