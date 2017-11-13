b5e6fc5577695867c3c515f9f682fb431914dcde
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-11-13 17:03:15 +0000

    specify site - now dealing with multiple sites in netcdf

--- src/rerun/python/analyse_GEMplot_rerun_DALEC_GSI_DFOL_CWD_FR.py
+++ src/rerun/python/analyse_GEMplot_rerun_DALEC_GSI_DFOL_CWD_FR.py
@@ -34,67 +34,67 @@ filename = 'BALI_GEMplots_daily_2011_2016_DALEC_GSI_DFOL_CWD_FR.nc'
 NetCDF_file = '%s%s/rerun/%s/%s' % (path2project,project,run,filename)
 mod = Dataset(NetCDF_file)
 
-site_index = 0
+site = 0
 # Get model output
 model = {}
 # carbon pools
-model['Cwoo']=mod.variables['Cwoo'][:,:,0,0]
-model['Croo']=mod.variables['Croo'][:,:,0,0]
-model['Clit']=mod.variables['Clit'][:,:,0,0]
-model['Csom']=mod.variables['Csom'][:,:,0,0]
-model['Ccwd']=mod.variables['Ccwd'][:,:,0,0]
-model['Clab']=mod.variables['Clab'][:,:,0,0]
-model['Cfol']=mod.variables['Cfol'][:,:,0,0]
-model['Cbio']=mod.variables['Cbio'][:,:,0,0]
+model['Cwoo']=mod.variables['Cwoo'][:,:,site]
+model['Croo']=mod.variables['Croo'][:,:,site]
+model['Clit']=mod.variables['Clit'][:,:,site]
+model['Csom']=mod.variables['Csom'][:,:,site]
+model['Ccwd']=mod.variables['Ccwd'][:,:,site]
+model['Clab']=mod.variables['Clab'][:,:,site]
+model['Cfol']=mod.variables['Cfol'][:,:,site]
+model['Cbio']=mod.variables['Cbio'][:,:,site]
 model['time']=mod.variables['time']
 
 # Litter/foliage related components
-model['lai']=mod.variables['lai'][:,:,0,0]
-model['flux_fol_lit']=mod.variables['flux_fol_lit'][:,:,0,0]
-model['flux_root_lit']=mod.variables['flux_root_lit'][:,:,0,0]
-model['flux_cwd_lit']=mod.variables['flux_cwd_lit'][:,:,0,0]
-model['flux_wood_cwd']=mod.variables['flux_wood_cwd'][:,:,0,0]
+model['lai']=mod.variables['lai'][:,:,site]
+model['flux_fol_lit']=mod.variables['flux_fol_lit'][:,:,site]
+model['flux_root_lit']=mod.variables['flux_root_lit'][:,:,site]
+model['flux_cwd_lit']=mod.variables['flux_cwd_lit'][:,:,site]
+model['flux_wood_cwd']=mod.variables['flux_wood_cwd'][:,:,site]
 
 # fluxes
-model['Reco']=mod.variables['Reco'][:,:,0,0]
-model['Rauto']=mod.variables['Rauto'][:,:,0,0]
-model['Rhet']=mod.variables['Rhet'][:,:,0,0]
-model['Rh_lit']=mod.variables['Rh_lit'][:,:,0,0]
-model['gpp']=mod.variables['gpp'][:,:,0,0]
-model['nee']=mod.variables['nee'][:,:,0,0]
-model['decomp_lit']=mod.variables['decomp_lit'][:,:,0,0]
+model['Reco']=mod.variables['Reco'][:,:,site]
+model['Rauto']=mod.variables['Rauto'][:,:,site]
+model['Rhet']=mod.variables['Rhet'][:,:,site]
+model['Rh_lit']=mod.variables['Rh_lit'][:,:,site]
+model['gpp']=mod.variables['gpp'][:,:,site]
+model['nee']=mod.variables['nee'][:,:,site]
+model['decomp_lit']=mod.variables['decomp_lit'][:,:,site]
 
 # GSI
-model['gsi']=mod.variables['gsi'][:,:,0,0]
-model['gsi_iphoto']=mod.variables['gsi_iphoto'][:,:,0,0]
-model['gsi_ivpd']=mod.variables['gsi_ivpd'][:,:,0,0]
-model['gsi_itemp']=mod.variables['gsi_itemp'][:,:,0,0]
+model['gsi']=mod.variables['gsi'][:,:,site]
+model['gsi_iphoto']=mod.variables['gsi_iphoto'][:,:,site]
+model['gsi_ivpd']=mod.variables['gsi_ivpd'][:,:,site]
+model['gsi_itemp']=mod.variables['gsi_itemp'][:,:,site]
 
 # Get corresponding observations
 obs_in = np.load('%s%s' % (data_dir,project_obs))
 obs_in[obs_in==-9999]=np.nan
 obs={}
-obs['time'] = 
-obs['gpp'] = obs_in[site_index,:,0]
-obs['gpp_u'] = obs_in[site_index,:,11]
-obs['nee'] = obs_in[site_index,:,2]
-obs['nee_u'] = obs_in[site_index,:,13]
-obs['lai'] = obs_in[site_index,:,1]
-obs['lai_u'] = obs_in[site_index,:,12]
-obs['Cwoo'] = obs_in[site_index,:,6]
-obs['Cwoo_u'] = obs_in[site_index,:,17]
-obs['Cfol'] = obs_in[site_index,:,5]
-obs['Cfol_u'] = obs_in[site_index,:,16]
-obs['Croo'] = obs_in[site_index,:,7]
-obs['Croo_u'] = obs_in[site_index,:,18]
-obs['Clit'] = obs_in[site_index,:,8]
-obs['Clit_u'] = obs_in[site_index,:,19]
-obs['Csom'] = obs_in[site_index,:,9]
-obs['Csom_u'] = obs_in[site_index,:,20]
-obs['Reco'] = obs_in[site_index,:,4]
-obs['Reco_u'] = obs_in[site_index,:,15]
-obs['flux_fol_lit'] = obs_in[site_index,:,32]
-obs['flux_fol_lit_u'] = obs_in[site_index,:,33]
+obs['time'] = mod.variables['time']
+obs['gpp'] = obs_in[site,:,0]
+obs['gpp_u'] = obs_in[site,:,11]
+obs['nee'] = obs_in[site,:,2]
+obs['nee_u'] = obs_in[site,:,13]
+obs['lai'] = obs_in[site,:,1]
+obs['lai_u'] = obs_in[site,:,12]
+obs['Cwoo'] = obs_in[site,:,6]
+obs['Cwoo_u'] = obs_in[site,:,17]
+obs['Cfol'] = obs_in[site,:,5]
+obs['Cfol_u'] = obs_in[site,:,16]
+obs['Croo'] = obs_in[site,:,7]
+obs['Croo_u'] = obs_in[site,:,18]
+obs['Clit'] = obs_in[site,:,8]
+obs['Clit_u'] = obs_in[site,:,19]
+obs['Csom'] = obs_in[site,:,9]
+obs['Csom_u'] = obs_in[site,:,20]
+obs['Reco'] = obs_in[site,:,4]
+obs['Reco_u'] = obs_in[site,:,15]
+obs['flux_fol_lit'] = obs_in[site,:,32]
+obs['flux_fol_lit_u'] = obs_in[site,:,33]
 
 # plot Carbon stocks
 pCAR.plot_carbon_pools_ts(model,obs)
