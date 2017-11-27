09a65ce87169e84df7a2bd9ff0dc3ee608e8dbc3
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-11-27 12:23:59 +0000

    run 005

--- src/setup/setup_BALI_GEMplots.py
+++ src/setup/setup_BALI_GEMplots.py
@@ -12,7 +12,7 @@ sys.path.append('/exports/csce/datastore/geos/users/dmilodow/BALI/CARDAMOM_BALI/
 import load_data as data
 
 # Project data file (to load in if already generated
-run = '004'
+run = '005'
 data_dir = "/home/dmilodow/DataStore_DTM/BALI/CARDAMOM_BALI/npydata/"
 if run not in os.listdir(data_dir):
     os.mkdir(data_dir + run)
