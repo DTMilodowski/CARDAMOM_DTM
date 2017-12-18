932f3932f1510154309ea1ee72daf164dce6ae24
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-12-18 16:55:46 +0000

    tidying

--- src/setup/obs/compile_CARDAMOM_obs_from_field_data.py
+++ src/setup/obs/compile_CARDAMOM_obs_from_field_data.py
@@ -47,21 +47,21 @@ N_t = date.size
 # Now deal with the obs
 for pp in range(0,len(plot)):
     print plot[pp]
-    Cwood_in = np.zeros(N_t)*np.nan#-9999.
-    Croot_in = np.zeros(N_t)*np.nan#-9999.
-    Croot_std_in = np.zeros(N_t)*np.nan#-9999.
+    Cwood_in = np.zeros(N_t)*np.nan
+    Croot_in = np.zeros(N_t)*np.nan
+    Croot_std_in = np.zeros(N_t)*np.nan
     root_npp_in = np.zeros(N_t)*np.nan
     root_npp_std_in = np.zeros(N_t)*np.nan
-    Csoil_in = np.zeros(N_t)*np.nan#-9999.
-    Litter_in = np.zeros(N_t)*np.nan#-9999.
-    Litter_std_in = np.zeros(N_t)*np.nan#-9999.
-    Litter_accumulation_days_in = np.zeros(N_t)*np.nan#-9999.
-    LAI_in = np.zeros(N_t)*np.nan#-9999.
-    LAI_std_in = np.zeros(N_t)*np.nan#-9999.
-    LAI_MH_in = np.zeros(N_t)*np.nan#-9999.
-    LAI_rad_in = np.zeros(N_t)*np.nan#-9999.
-    LAI_rad_std_in = np.zeros(N_t)*np.nan#-9999.
-    LAI_MH_std_in = np.zeros(N_t)*np.nan#-9999.
+    Csoil_in = np.zeros(N_t)*np.nan
+    Litter_in = np.zeros(N_t)*np.nan
+    Litter_std_in = np.zeros(N_t)*np.nan
+    Litter_accumulation_days_in = np.zeros(N_t)*np.nan
+    LAI_in = np.zeros(N_t)*np.nan
+    LAI_std_in = np.zeros(N_t)*np.nan
+    LAI_MH_in = np.zeros(N_t)*np.nan
+    LAI_rad_in = np.zeros(N_t)*np.nan
+    LAI_rad_std_in = np.zeros(N_t)*np.nan
+    LAI_MH_std_in = np.zeros(N_t)*np.nan
 
     # LAI data
     LAI_date, LAI, LAI_std = field.get_LAI_ts(LAI_file,plot[pp])
